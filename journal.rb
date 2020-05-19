306750c74cef872d5dbf12c9f0b18734e53eefb0
Mike McClenaghan <mike@sideline.ca>
2020-05-19 14:30:34 -0600

    Added weekly template to journal script

--- journal.rb
+++ journal.rb
@@ -6,6 +6,8 @@ today = Date.today
 filename = today.strftime('%Y-%m-%d.md')
 last_year = today.prev_year.strftime('%Y-%m-%d')
 this_week = today.strftime('%G-W%V')
+this_week_monday = today - (today.cwday-1)
+week_filename = this_week + '.md'
 
 weather_raw = `curl -s https://weather.gc.ca/rss/city/ab-50_e.xml`
 today_weather_raw = weather_raw.split('<entry>').slice(3..4).join
@@ -28,4 +30,43 @@ JOURNAL
 
 File.open("#{filepath}#{filename}", 'a') { |f| f.write "#{journal_template}" }
 
-puts "Wrote template to #{filepath}#{filename}"
+puts "Wrote daily template to #{filepath}#{filename}"
+
+weekly_template = <<~WEEKLY
+  # #{this_week}
+
+  ## Big Three
+    1. 
+    2. 
+    3. 
+
+  ## Daily stuff
+    - Monday, [[#{this_week_monday.strftime('%Y-%m-%d')}]]
+    - Tuesday, [[#{this_week_monday.next_day.strftime('%Y-%m-%d')}]]
+    - Wednesday, [[#{this_week_monday.next_day(2).strftime('%Y-%m-%d')}]]
+    - Thursday, [[#{this_week_monday.next_day(3).strftime('%Y-%m-%d')}]]
+    - Friday, [[#{this_week_monday.next_day(4).strftime('%Y-%m-%d')}]]
+    - Saturday, [[#{this_week_monday.next_day(5).strftime('%Y-%m-%d')}]]
+    - Sunday, [[#{this_week_monday.next_day(6).strftime('%Y-%m-%d')}]]
+    
+  ## Last week [[#{(today-7).strftime('%G-W%V')}]]
+    - Tasks:
+        - [ ] Add meetings from last week to contacts and events
+        - [ ] Make sure all [[TODO]] items from last week were either closed or moved
+        - [ ] Make sure there are no recurring items that somehow got marked as [[DONE]]
+    - Biggest wins:
+        - 
+    - How far did you get on last week's big 3?
+      1. 
+      2. 
+      3. 
+    - What worked? What didn't?
+      - 
+    - What will you keep, improve, start, or stop based on the above?
+      - 
+WEEKLY
+
+unless File.exist?("#{filepath}#{week_filename}") 
+  File.open("#{filepath}#{week_filename}", 'w') { |f| f.write "#{weekly_template}" }
+  puts "Wrote weekly template to #{filepath}#{week_filename}"
+end
