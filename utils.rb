093046642d9df4fcf7ab0ad6f977e03063740868
Abderahmane Bouziane <bobmatt911@gmail.com>
2021-02-13 15:16:16 -0500

    remove output in normal flow of script

--- utils.rb
+++ utils.rb
@@ -6,13 +6,11 @@ def get_notes_path_from_cli_args()
   else
     notes_path = Pathname.new("#{ENV['HOME']}/Notes/")
   end
-  
+
   if not notes_path.directory?
     Kernel.abort("Directory [#{notes_path}] does not exist.
 Please specify your notes directory as a command line argument.
    ex: ruby todo.md ~/john/my_notes")
-  else
-    puts "notes path: #{notes_path}".inspect
   end
 
   return notes_path
